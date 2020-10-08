1bf0134425e0050a95ae2905bce34b2ddb8c6ebf
toleen98 <toleenballool98@gmail.com>
2020-10-08 21:53:41 +0300

    structure files

---
+++ src/pages/detailedPostPage.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+
+class Detailedpost extends Component {
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default Detailedpost;
