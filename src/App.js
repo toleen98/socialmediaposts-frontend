367e78816763d2dac46b3aee57213a9d7b16a1c1
toleen98 <toleenballool98@gmail.com>
2020-10-10 02:40:31 +0300

    register page / create regeister form

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import NavbarLayout from './layouts/Navbar/Navbar';
 import {} from 'react-router-dom'
 import Posts from './pages/PostsPage'
 import CreatePost from './pages/CreatePostPage'
-
+import Register from './pages/auth/Register'
 
 import { BrowserRouter as Router, Switch,  Route } from 'react-router-dom';
 
@@ -15,8 +15,6 @@ import { BrowserRouter as Router, Switch,  Route } from 'react-router-dom';
 function App() {
   return (
     <div>
-      
-      
         <Router >
           <NavbarLayout/>
           <Row>
@@ -25,7 +23,7 @@ function App() {
             <Col xs={12} md={6}>
               <Switch>
                 <Route path="/" exact  component={Posts}/>
-                <Route path="/createpost" component={CreatePost}/>
+                <Route path="/createpost" component={Register}/>
               </Switch>
             </Col>
             <Col md={3}></Col>
--- src/layouts/Navbar/Navbar.js
+++ src/layouts/Navbar/Navbar.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import { Navbar } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 
 class NavbarLayout extends Component {
     render() {
         return (
             
             <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="#home">
-              {' '}
-              React Bootstrap
-            </Navbar.Brand>
+            <Link to='/'>
+              <Navbar.Brand >
+                {' '}
+                React Bootstrap
+              </Navbar.Brand>
+            </Link>
+           
           </Navbar>
         )
            
