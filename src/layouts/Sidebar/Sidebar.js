946a1fc81b675595960dc9cc18a1f23a866cb9c9
toleen98 <toleenballool98@gmail.com>
2020-10-11 00:12:10 +0300

    sidebar layout/ make create post and user name show to authinticated users

--- src/layouts/Sidebar/Sidebar.js
+++ src/layouts/Sidebar/Sidebar.js
@@ -1,16 +1,46 @@
 import React,{Component} from 'react';
 import SideBarItem from './SidebarItem/SidebarItem';
-import {Menu} from 'semantic-ui-react';
+import {Menu, Image} from 'semantic-ui-react';
 import './Sidebar.scss';
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import userimg from '../../assets/imges/userimg.png'
+import './SidebarItem/SideBarItem.scss'
+
 
 class Sidebar extends Component {
-  
+  state = {
+    logged : false,
+    userName :''
+  }
+
+  componentWillMount() {
+    
+    if (this.props.auth.isAuthenticated) {
+      // push user to home when they login
+      this.setState({logged:true, userName: this.props.auth.user.name })
+    }
+   
+  }
+
   render() {
     return (
       
         <Menu borderless vertical stackable fixed='left' className='side-nav' >
+          {this.state.logged && <Menu.Item className='sidebar-item'>
+            <div className='sidebar-item-alignment-container'>
+              <Image
+               floated='left'
+               size='mini'
+               src={userimg}
+               />
+               <span>{this.state.userName.toUpperCase()}</span>
+              </div> 
+            
+          </Menu.Item>}
+          <br/>
           <SideBarItem highlight={true} path='/'  label='Home' icon='home'/>
-          <SideBarItem path='/createpost' label='Creat Post' icon='write'/>
+          {this.state.logged && <SideBarItem path='/createpost' label='Creat Post' icon='write'/>}
           <SideBarItem label='Report history' icon='flag'/>
           <SideBarItem label='Help' icon='help circle'/>
           <SideBarItem label='Send feedback' icon='comment'/>
@@ -21,4 +51,14 @@ class Sidebar extends Component {
   }
 }
 
-export default Sidebar;
+Sidebar.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps
+)(Sidebar);
